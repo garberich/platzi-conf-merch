6a86297973bfbe7de1be73ade62c3857f5c425a5
Richard <rgarcia@ubits.co>
2021-10-31 22:04:19 -0500

    Final commit

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -7,12 +7,16 @@ const Products = () => {
   // const { state, addToCart } = useContext(AppContext);
   // const { products } = state;
 
-  const { state: {products}, addToCart} = useContext(AppContext);
+  // Como ya los productos no vienen del initialState sino de Strapi entonces la destructuracion cambia
+  // const { state: {products}, addToCart} = useContext(AppContext);
+
+  const { products, addToCart } = useContext(AppContext);
 
   const handleAddToCart = product => () => {
     addToCart(product)
   }
 
+  console.log(products)
   return (
   <div className="Products">
     <div className="Products-items">
