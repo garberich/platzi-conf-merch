9525ba220a4bd07a8fcb56abcd3c9cbca0efe7da
Richard <rgarcia@ubits.co>
2021-10-31 20:51:17 -0500

    [add] react-helmet

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,52 +1,67 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 import AppContext from '../context/AppContext';
 import '../styles/components/Checkout.css';
 
 const Checkout = () => {
-    const { state: { cart }, removeFromCart } = useContext(AppContext);
+  const {
+    state: { cart },
+    removeFromCart,
+  } = useContext(AppContext);
 
-    const handleRemove = (product, i) => () => {
-        removeFromCart(product, i);
-    };
+  const handleRemove = (product, i) => () => {
+    removeFromCart(product, i);
+  };
 
-    /* 
+  /* 
         Vamos a sumar todos los elementos que tenemos
         No pasamos ningun valor porque vamos a usar cart
         Creamos el reducer para usarlo en cart
     */
-    const handleSumTotal = () => {
-        const reducer = (accumulator, currentValue) => accumulator + currentValue.price;
-        const sum = cart.reduce(reducer, 0);
-        return sum;
-    }
+  const handleSumTotal = () => {
+    const reducer = (accumulator, currentValue) =>
+      accumulator + currentValue.price;
+    const sum = cart.reduce(reducer, 0);
+    return sum;
+  };
 
-    return (
-    <div className="Checkout">
+  return (
+    <>
+      <Helmet>
+        <title>Lista de pedidos - Platzi Conf Merch</title>
+      </Helmet>
+      <div className="Checkout">
         <div className="Checkout-content">
-            {cart.length > 0 ? <h3>Lista de pedidos:</h3> : <h3>Sin pedidos...</h3>}
-            {cart.map((item, i) => (
-                <div className="Checkout-item">
-                    <div className="Checkout-element">
-                        <h4>{item.title}</h4>
-                        <span>${item.price}</span>
-                    </div>
-                    <button type="button" onClick={handleRemove(item, i)}>
-                        <i className="fas fa-trash-alt" />
-                    </button>
-                </div>
-            ))}
+          {cart.length > 0 ? (
+            <h3>Lista de pedidos:</h3>
+          ) : (
+            <h3>Sin pedidos...</h3>
+          )}
+          {cart.map((item, i) => (
+            <div className="Checkout-item">
+              <div className="Checkout-element">
+                <h4>{item.title}</h4>
+                <span>${item.price}</span>
+              </div>
+              <button type="button" onClick={handleRemove(item, i)}>
+                <i className="fas fa-trash-alt" />
+              </button>
+            </div>
+          ))}
         </div>
         {cart.length > 0 && (
-            <div className="Checkout-sidebar">
-                <h3>{`Precio Total: ${handleSumTotal()}`}</h3>
-                <Link to="/checkout/information">
-                    <button type="button">Continuar pedido</button>
-                </Link>
-            </div>
+          <div className="Checkout-sidebar">
+            <h3>{`Precio Total: ${handleSumTotal()}`}</h3>
+            <Link to="/checkout/information">
+              <button type="button">Continuar pedido</button>
+            </Link>
+          </div>
         )}
-    </div>
-)};
+      </div>
+    </>
+  );
+};
 
 export default Checkout;
